3f22ade0f270f730e00c9969d3e16f18b33e71ae
doucos <youssouph.doucoure95@gmail.com>
2018-03-13 11:06:08 +0100

    first_commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const app = express();
+
+app.get('*', (req, res) => {
+    res.send('Le Real Madrid est le meilleur club du monde')
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(3000, (err) => {
+    if(err) {
+       return console.log('error connection')
+    }
+
+    console.log('connected')
+})
